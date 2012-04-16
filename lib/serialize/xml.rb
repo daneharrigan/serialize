9aaa7f49646c7460d5fef7a263a3299dbcdbfdd0
Dane Harrigan <dane.harrigan@gmail.com>
2012-04-16 00:34:38 -0700

    fixed outer xml tag naming

--- lib/serialize/xml.rb
+++ lib/serialize/xml.rb
@@ -3,9 +3,24 @@ class Serialize
     def render(*args)
       # args are empty unless called through
       # another to_xml call
-      options = args.empty? ? { :root => object.class.name.titleize.downcase } : args.first
+      options = unless args.empty?
+        args.first
+      else
+        subject = collection? ? object.first.class.name : object.class.name
+        name = subject.include?("::") ? subject.split("::").last : subject
+        name = name.titleize.downcase
+        name = name.pluralize if collection?
+        { :root => name }
+      end
+
       content.to_xml(options)
     end
+
+    def collection?
+      object.respond_to? :map
+    end
+
+    private :collection?
   end
 end
 
